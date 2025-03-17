ef19902c849af9c2560219aca57b9ee8e3b48489
Apurba1903 <apurba1903@gmail.com>
2025-03-17 08:39:11 +0600

    block

---
+++ local_scope.go
@@ -0,0 +1,25 @@
+package main
+import "fmt"
+
+var(
+	a = 20
+	b = 30
+)
+
+/*
+	1. Block -> {}
+*/
+
+func main(){
+
+	x := 18
+
+	if x >= 18 {
+
+		p := 10
+
+		fmt.Println("I am mature.")
+		fmt.Println("I have", p , "friends.")
+	}
+
+}
